wb2osz/direwolf

commit b26c5a4d7d4b2fd19160a1ff20cece13b2c18b13

Improve error message.

src/deviceid.c
@@ -528,7 +528,7 @@ static int mice_cmp (const void *px, const void *py)
 
 void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 {
-	*device = '\0';
+	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 	if (ptocalls == NULL) {
 	  text_color_set(DW_COLOR_ERROR);
@@ -554,6 +554,7 @@ void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 	  }
 	}
 
+// Not found in table.
 	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 } // end deviceid_decode_dest
@@ -610,7 +611,7 @@ static inline int strncmp_z (char *a, char *b, size_t len)
 
 void deviceid_decode_mice (char *comment, char *trimmed, size_t trimmed_size, char *device, size_t device_size)
 {
-	*device = '\0';
+	strlcpy (device, "UNKNOWN vendor/model", device_size);
 
 	if (ptocalls == NULL) {
 	  text_color_set(DW_COLOR_ERROR);

doismellburning/samoyed

commit ffbb6d5ffe608241144ffb80320376ad49129e60

Fix error message in deviceid_decode_dest
Per https://github.com/wb2osz/direwolf/issues/577 and
https://github.com/wb2osz/direwolf/pull/578, looks like a small bug with
error message logic was introduced in
b26c5a4d7d4b2fd19160a1ff20cece13b2c18b13

src/deviceid.c
@@ -391,6 +391,10 @@ void deviceid_decode_dest (char *dest, char *device, size_t device_size)
 	      strlcat (device, " ", device_size);
 	    }
 
+	    if (ptocalls[n].vendor == NULL && ptocalls[n].model != NULL) {
+	      strlcpy (device, "", device_size);
+	    }
+
 	    if (ptocalls[n].model != NULL) {
 	      strlcat (device, ptocalls[n].model, device_size);
 	    }