5fb1bec9bec0c429a2a07bb9635e46c5e3dbd5c0
Kristian Glass <git@doismellburning.co.uk>
2025-07-25 13:24:48 +0100

    Add shims for text_color_set and DW_COLOR_ERROR

--- src/cgo_shims.go
+++ src/cgo_shims.go
@@ -3,14 +3,21 @@ package direwolf
 // Assorted utilities when porting from C to Go
 
 // #include "decode_aprs.h"
+// #include "textcolor.h"
 import "C"
 
 import "fmt"
 
 const G_UNKNOWN = C.G_UNKNOWN
 
+const DW_COLOR_ERROR = C.DW_COLOR_ERROR
+
 func dw_printf(format string, a ...any) (int, error) {
 	// Can't call variadic functions through cgo, so let's define our own!
 	// Fortunately dw_printf doesn't do much
 	return fmt.Printf(format, a...)
+}
+
+func text_color_set(c C.enum_dw_color_e) {
+	C.text_color_set(C.dw_color_t(c))
 }
