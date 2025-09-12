1ff84508281f76687385490f9491a56b476e2d7f
Kristian Glass <git@doismellburning.co.uk>
2025-09-12 23:37:52 +0100

    Add AGWPEMessage

--- src/agwpe.go
+++ src/agwpe.go
@@ -1,5 +1,10 @@
 package direwolf
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 type AGWPEHeader struct {
 	Portx        byte
 	Reserved1    byte
@@ -13,4 +18,23 @@ type AGWPEHeader struct {
 	CallTo       [10]byte
 	DataLen      uint32
 	UserReserved [4]byte
+}
+
+type AGWPEMessage struct {
+	Header AGWPEHeader
+	Data   []byte
+}
+
+// binary.Write won't send variable-length slices, and I keep forgetting that, so...
+func (msg *AGWPEMessage) Write(w io.Writer, order binary.ByteOrder) (int, error) {
+	var headerErr = binary.Write(w, order, msg.Header)
+	if headerErr != nil {
+		return 0, headerErr
+	}
+
+	if msg.Header.DataLen > 0 {
+		return w.Write(msg.Data)
+	}
+
+	return 0, nil
 }
