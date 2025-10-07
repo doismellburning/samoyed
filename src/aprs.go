9c1d49126c44ffdcfbefcbfe8f31541c11ba4478
Kristian Glass <git@doismellburning.co.uk>
2025-10-07 23:23:41 +0100

    Export the APRS position struct feels to support encode/binary

--- src/aprs.go
+++ src/aprs.go
@@ -1,22 +1,22 @@
 package direwolf
 
 type position_t struct {
-	lat          [8]byte
-	sym_table_id byte /* / \ 0-9 A-Z */
-	lon          [9]byte
-	symbol_code  byte
+	Lat        [8]byte
+	SymTableId byte /* / \ 0-9 A-Z */
+	Lon        [9]byte
+	SymbolCode byte
 }
 
 type compressed_position_t struct {
-	sym_table_id byte /* / \ a-j A-Z */
+	SymTableId byte /* / \ a-j A-Z */
 	/* "The presence of the leading Symbol Table Identifier */
 	/* instead of a digit indicates that this is a compressed */
 	/* Position Report and not a normal lat/long report." */
 
-	y           [4]byte /* Compressed Latitude. */
-	x           [4]byte /* Compressed Longitude. */
-	symbol_code byte
-	c           byte /* Course/speed or radio range or altitude. */
-	s           byte
-	t           byte /* Compression type. */
+	Y          [4]byte /* Compressed Latitude. */
+	X          [4]byte /* Compressed Longitude. */
+	SymbolCode byte
+	C          byte /* Course/speed or radio range or altitude. */
+	S          byte
+	T          byte /* Compression type. */
 }
