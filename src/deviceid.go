d4bf4736f2d7a8384fb86044ee61ff59c79add46
Kristian Glass <git@doismellburning.co.uk>
2025-10-21 20:25:14 +0100

    More

--- src/deviceid.go
+++ src/deviceid.go
@@ -1,26 +1,6 @@
-//
-//    This file is part of Dire Wolf, an amateur radio packet TNC.
-//
-//    Copyright (C) 2023  John Langner, WB2OSZ
-//
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-
+package direwolf
 
 /*------------------------------------------------------------------
- *
- * File:	deviceid.c
  *
  * Purpose:	Determine the device identifier from the destination field,
  *		or from prefix/suffix for MIC-E format.
@@ -30,16 +10,14 @@
  *
  *------------------------------------------------------------------*/
 
-#include "direwolf.h"
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-
-#include "deviceid.h"
-#include "textcolor.h"
-
+// #include "direwolf.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <assert.h>
+// #include "deviceid.h"
+// #include "textcolor.h"
+import "C"
 
 static void unquote (int line, char *pin, char *pout);
 static int tocall_cmp (const void *px, const void *py);
