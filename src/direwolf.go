9f3e531be4c6453ba5d2fb4e6a98fa5227e82404
Kristian Glass <git@doismellburning.co.uk>
2026-01-17 21:39:34 +0000

    refactor: Remove dns_sd_dw.h

--- src/direwolf.go
+++ src/direwolf.go
@@ -32,7 +32,6 @@ package direwolf
 // #include "dwgps.h"
 // #include "ax25_link.h"
 // #include "fx25.h"
-// #include "dns_sd_dw.h"
 // #include "dlq.h"		// for fec_type_t definition.
 // #cgo pkg-config: alsa avahi-client hamlib libbsd-overlay libgpiod libudev
 // #cgo CFLAGS: -I../external/geotranz -DMAJOR_VERSION=0 -DMINOR_VERSION=0 -DUSE_CM108 -DUSE_AVAHI_CLIENT -DUSE_HAMLIB -DUSE_ALSA
