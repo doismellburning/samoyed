0cef50d7fcc5ec48c6134c4e1747454932b6d0a8
Kristian Glass <git@doismellburning.co.uk>
2026-02-08 11:24:31 +0000

    refactor: Use Go types in fxrec.go and drop obsolete C import

--- src/fxrec.go
+++ src/fxrec.go
@@ -1,16 +1,10 @@
 package direwolf
 
-// #include <stdio.h>
-// #include <assert.h>
-// #include <string.h>
-// #include <stdint.h>
-// #include <stdlib.h>
-import "C"
-
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
-	"unsafe"
 )
 
 func FxrecMain() {
@@ -18,24 +12,34 @@ func FxrecMain() {
 
 	fx25_init(3)
 
-	var i C.int
-	for i = CTAG_MIN; i <= CTAG_MAX; i++ {
+	for i := CTAG_MIN; i <= CTAG_MAX; i++ {
 		var fname = fmt.Sprintf("fx%02x.dat", i)
-		var fp = C.fopen(C.CString(fname), C.CString("rb"))
-		if fp == nil {
-			fmt.Printf("****** Could not open %s ******\n", fname)
+		var fp, err = os.Open(fname) //nolint:gosec
+		if err != nil {
+			fmt.Printf("****** Could not open %s: %s ******\n", fname, err)
 			fmt.Printf("****** Did you generate the test files first? ******\n")
 			os.Exit(1)
 		}
 
-		var ch C.uchar
-		for C.fread(unsafe.Pointer(&ch), 1, 1, fp) == 1 {
-			var imask C.uchar
+		var buf = make([]byte, 1)
+		for {
+			var _, readErr = fp.Read(buf)
+			if readErr != nil && !errors.Is(readErr, io.EOF) {
+				fmt.Printf("Error reading %s: %s.\n", fname, err)
+				break
+			}
+			var ch = buf[0]
+			var imask byte
 			for imask = 0x01; imask != 0; imask <<= 1 {
 				fx25_rec_bit(0, 0, 0, int(ch&imask))
 			}
+
+			if errors.Is(readErr, io.EOF) {
+				break
+			}
 		}
-		C.fclose(fp)
+
+		fp.Close()
 	}
 
 	if fx25_test_count == 11 {
